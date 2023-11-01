cb8054be0af2fd8e9053c8e2d71b20d7a868ab94
AnirudhArrepu <144601688+AnirudhArrepu@users.noreply.github.com>
2023-11-01 22:28:06 +0530

    Update RailFence.py

--- RailFence.py
+++ RailFence.py
@@ -27,12 +27,17 @@ def encryption(key, string):
 
 
 def decryption(key, encrypted_string):
-    arr = [["" in range(3)] in range(len(encrypted_string))]
+    arr = [["" for x in range(len(encrypted_string))] for y in range(key)]
     k = 0
+    dec = 2*(key-1)
     for i in range(3):
-        for j in range(i, len(encrypted_string), 2 * key - 2):
+        for j in range(i, len(encrypted_string), dec):
             arr[i][j] = encrypted_string[k]
             k += 1
+        dec = dec - 2
+        if dec == 0:
+            dec = 2 * (key - 1)
+
     i = 0
     odd = True
     even = False
@@ -44,14 +49,12 @@ def decryption(key, encrypted_string):
             if i == key - 1:
                 odd = False
                 even = True
-                i = key - 1
         elif even:
             decrypted += arr[i][j]
             i -= 1
             if i == 0:
                 odd = True
                 even = False
-                i = 0
     return decrypted
 
 
